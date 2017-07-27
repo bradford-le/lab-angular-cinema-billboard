5cb579a497775d30a7b64bcb5807ac7458207a5c
Brad Le <brad.le444@gmail.com>
2017-07-27 17:40:20 +0200

    Movie Detail Working

--- starter-code/src/app/components/my-movie/my-movie.component.ts
+++ starter-code/src/app/components/my-movie/my-movie.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Router} from '@angular/router';
+import {MyCinemaServiceService} from "../../services/my-cinema-service.service";
 
 
 @Component({
   selector: 'app-my-movie',
   templateUrl: './my-movie.component.html',
-  styleUrls: ['./my-movie.component.css']
+  styleUrls: ['./my-movie.component.css'],
+  providers: [MyCinemaServiceService]  //DON'T FORGET TO ADD SERVICE PROVIDERS!!!!!
 })
 export class MyMovieComponent implements OnInit {
 
-  constructor(private route:ActivatedRoute, private router:Router) { }
+  constructor(
+    private route:ActivatedRoute, 
+    private router:Router,
+    private cinemaService: MyCinemaServiceService
+  ) { }
 
-  movieID: Number;
+  movie: any;
 
   ngOnInit() {
     this.route.params
-      .subscribe((params)=> this.movieID = Number(params['id']));
+      .subscribe((params)=> this.movie = this.cinemaService.getMovie(Number(params['id'])));
   }
 
   goToHome(){
+    console.log("HOME CLICKED!");
     this.router.navigate(['home']);
   }
 }
